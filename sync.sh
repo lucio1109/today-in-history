fc0beb28164d18ca5b708fa4075af35884a6fc24
gfw-breaker <lixiaoming0513@gmail.com>
2018-10-29 16:30:29 +0800

    ok

--- sync.sh
+++ sync.sh
@@ -33,16 +33,60 @@ cat > $index_page << EOF
 <body>
 EOF
 
-for v in $(ls -t *.mp4); do
-	name=$(echo $v | sed "s/'//g")
-	mv $v $name
-done
+ls -t *.mp4 > list.txt
+
+while read v; do
+	name=$(echo $v | sed "s/'//g" | sed "s/ /_/g" )
+	mv "$v" "$name" > /dev/null 2>&1
+done < list.txt
 
 for v in $(ls -t *.mp4); do
-	title=$(echo $v | cut -c6- )
-	name=$(echo $v | sed "s/'//g")
-	echo "<a href='http://$ip/history/$name?t=$ts'>$title</a></br></br>" >> $index_page
-	echo "##### <a href='http://$ip/history/$name?t=$ts'>$title</a>" >> $md_page
+	title=$(echo $v | cut -c6-)
+	name=$v
+	echo "<a href='http://$ip/history/$name.html?t=$ts'><b>$title</b></a></br></br>" >> $index_page
+	echo "##### <a href='http://$ip/history/$name.html?t=$ts'>$title</a>" >> $md_page
+	cat > $video_dir/$name.html << EOF
+<head>
+<title> $title </title>
+<meta charset="UTF-8"> 
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel='stylesheet' id='videojs-css' href='https://unpkg.com/video.js@6.7.3/dist/video-js.min.css' type='text/css' media='all' />
+<style>
+#player {
+	margin: 0 auto;
+	margin-top: 20px;
+	width: 100%;
+	max-width: 640px;
+}
+</style>
+</head>
+<br/>
+<h4><center>$title</center></h4>
+<center>
+<video id="player" controls autoplay preload="auto">
+	<source src="$name?t=$ts" type="video/mp4">
+</video>
+<p>
+<a href="https://github.com/gfw-breaker/nogfw/blob/master/README.md" target="_blank"><b> 一键翻墙软件</b></a>&nbsp;&nbsp;
+<a href="http://$ip:10080" target="_blank"><b>大纪元新闻网</b></a>&nbsp;&nbsp;
+<a href="http://$ip:8000" target="_blank"><b>新唐人电视台</b></a>&nbsp;&nbsp;
+<br/><br/>
+<a href="http://$ip:10080/gb/8/11/24/n2339512.htm"><b>文昭：生活在希望中，做快乐的中国人</b></a>
+<br/><br/>
+<a href="http://$ip:10000/videos/blog/tuid.html"><b>三退大潮席卷全球 三亿人觉醒见证中共末日</b></a>
+<br/><br/>
+<a href="http://$ip:10000/videos/jiuping/index.html"><b>《九评共产党》</b></a>&nbsp;&nbsp;
+<a href="http://$ip:10000/videos/mtdwh/index.html"><b>《漫谈党文化》</b></a>&nbsp;&nbsp;
+<a href="http://$ip:8000/xtr/gb/prog99.html"><b>《热点互动直播》</b></a>&nbsp;&nbsp;
+<a href="http://$ip:8000/xtr/gb/prog109.html"><b>《大陆新闻解读》</b></a>&nbsp;&nbsp;
+<br/><br/>
+<a href="http://$ip:10000/videos/blog/weihuo.html"><b>天安门自焚真相《伪火》</b></a>&nbsp;&nbsp;
+<a href="http://$ip:10000/videos/blog/425event.html"><b>“四‧二五”中南海万人上访始末</b></a>&nbsp;&nbsp;
+<br/><br/>
+</p>
+</center>
+EOF
+
 done
 echo "</body></html>" >> $index_page
 
@@ -54,8 +98,8 @@ cd /root/today-in-history
 git pull
 sed -i '5,$d' README.md
 cat $md_page >> README.md
-sed -i "10 a$vlinks" README.md
-sed -i "10 a$plinks" README.md
+sed -i "8 a$vlinks" README.md
+sed -i "8 a$plinks" README.md
 git commit -a -m 'ok'
 git push
 
