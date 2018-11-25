df3d16d90f5c2a43acd9c71f5411e9632cc6c967
gfw-breaker <lixiaoming0513@gmail.com>
2018-11-25 12:24:04 +0800

    ok

--- sync.sh
+++ sync.sh
@@ -11,7 +11,7 @@ dl_script=https://raw.githubusercontent.com/gfw-breaker/youtube-video/master/dl.
 # download
 cd /root/$folder
 wget -q $dl_script -O dl.sh
-bash dl.sh $folder $youtube_url $stick
+bash dl.sh -f $folder -u $youtube_url -s $stick -a
 
 
 # push
